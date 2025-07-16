92774e53bd241254cc90d5e8cc10cd21c604c718
ImrulIbtehaz <orvee.irmul32@gmail.com>
2025-07-16 09:19:05 +0600

    Sanctum Setup

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    private $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        $url = url('/reset-password/' . $this->token . '?email=' . urlencode($notifiable->email));
+
+        return (new MailMessage)
+            ->subject('Reset Password Notification')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', $url)
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\EmailVerificationController;
+use App\Http\Controllers\Api\Auth\ForgotPasswordController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\ResetPasswordController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::post('/register',[RegisterController::class, 'register']);
+Route::post('/login',[LoginController::class, 'login']);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/logout',[LogoutController::class, 'logout']);
+    Route::post('/send-otp', [EmailVerificationController::class, 'sendOtp']);
+   Route::post('/verify-otp',[EmailVerificationController::class, 'verifyOtp']);
+});
+Route::post('password/forgot', [ForgotPasswordController::class, 'sendResetOTP']);
+Route::post('password/reset', [ResetPasswordController::class, 'reset']);
